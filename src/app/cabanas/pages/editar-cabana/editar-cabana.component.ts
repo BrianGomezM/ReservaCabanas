0a77a380f6f4df43cb54d45a14f8fbeaccfaff67
kquintana <cristaldemilcolores@gmail.com>
2022-05-20 22:24:07 -0500

    añadir editar-cabañas

--- src/app/cabanas/pages/editar-cabana/editar-cabana.component.ts
+++ src/app/cabanas/pages/editar-cabana/editar-cabana.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { Cabana } from 'app/cabanas/interfaces/Cabana.interface';
 import { CabanasService } from 'app/cabanas/services/cabanas.service';
 
 @Component({
@@ -8,13 +9,33 @@ import { CabanasService } from 'app/cabanas/services/cabanas.service';
   styleUrls: ['./editar-cabana.component.css']
 })
 export class EditarCabanaComponent implements OnInit {
+  nombre:string;
+  @Output() cabanas: Cabana[] = [];
+  cabana:Cabana={
+     id_cabana:"",
+     nombre_cabana:"",
+     descripcion_cabana:"",
+     capacidad_cabana:"",
+     valor_cabana:"",
+     estado:1
+  };
 
   constructor(private router: Router,private cabanaService:CabanasService) { }
 
   ngOnInit(): void {
   }
+  
   redirect(){
     this.router.navigate(['/cabanas']);
+  }
+
+  actualizarCabana(){
+    this.cabanaService.actualizarCabana(this.cabana).subscribe(
+      resp=>{
+        console.log(resp);
+        this.redirect();
+      }
+    )
   }
 
 }
