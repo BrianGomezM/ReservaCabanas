d1ca17de584746448fb00552aefc8648b0cef1a5
kquintana <cristaldemilcolores@gmail.com>
2022-05-22 05:17:39 -0500

    pages: Editar cabañas - interface: tipo de dato de capacidad_cabana

--- src/app/cabanas/cabanas.module.ts
+++ src/app/cabanas/cabanas.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ListadoCabanasComponent } from './pages/listado-cabanas/listado-cabanas.component';
 import { CrearCabanaComponent } from './pages/crear-cabana/crear-cabana.component';
 import { EditarCabanaComponent } from './pages/editar-cabana/editar-cabana.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 
 
