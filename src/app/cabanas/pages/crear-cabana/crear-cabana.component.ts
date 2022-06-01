00188164e1c8948229ea8fb1fd41f5fb72c9a663
unknown <cristaldemilcolores@gmail.com>
2022-06-01 16:30:46 -0500

    Imagenes restricciones y eliminar

--- src/app/cabanas/pages/crear-cabana/crear-cabana.component.ts
+++ src/app/cabanas/pages/crear-cabana/crear-cabana.component.ts
@@ -3,7 +3,7 @@ import { Cabana } from 'app/cabanas/interfaces/Cabana.interface';
 import { Imagen } from 'app/cabanas/interfaces/imagenes.interface';
 import { CabanasService } from 'app/cabanas/services/cabanas.service';
 import { Router } from '@angular/router';
-
+declare var $: any;
 @Component({
   selector: 'app-crear-cabana',
   templateUrl: './crear-cabana.component.html',
@@ -58,22 +58,39 @@ export class CrearCabanaComponent implements OnInit {
       this.onCrear.emit();
     })
   }
+  typeValidate(file:File){
+     let varResultado:Boolean=false;
+    if(file.type == "image/png" || file.type == "image/jpg"){
+        varResultado=true;
+    }
+    return varResultado;
+  }
+
+  deleteImage(i:number){
+    delete this.fileToUpload[i];
+  }
+
   cargarImagenes(file:FileList){
     for(let i=0; i<file.length;i++){
-      //reader.onload = ; // Renderizamos la imagen
-      let reader = new FileReader();
-     
-      reader.onloadend = () => {
-        this.imagenes.push(file.item(i));
-        this.imagen={
-          id_imagen:"",
-          nombre_imagen:file.item(i).name,
-          url_imagen:reader.result as string
-        };
-        this.fileToUpload.push(this.imagen);
+      if(this.typeValidate(file.item(i))){
+        //reader.onload = ; // Renderizamos la imagen
+        let reader = new FileReader();
+      
+        reader.onloadend = () => {
+          this.imagenes.push(file.item(i));
+          this.imagen={
+            id_imagen:"",
+            nombre_imagen:file.item(i).name,
+            url_imagen:reader.result as string
+          };
+          this.fileToUpload.push(this.imagen);
+        }
+        reader.readAsDataURL(file.item(i));
+        console.log(reader.result);
+      }else{
+        this.showNotification("top", "right", 1, "ERROR:", "solo se admiten archivos en formatos de imagen (.PNG  y .JPG)");
+        
       }
-      reader.readAsDataURL(file.item(i));
-      console.log(reader.result);
     }
   }
   
@@ -93,6 +110,32 @@ export class CrearCabanaComponent implements OnInit {
     }
    
     }
+
+    showNotification(from, align, opcion, titulo, mesanje){
+      const type = ['success','danger'];
+      const color = opcion;
+      $.notify({
+          icon: titulo,
+          message: mesanje
+      },{
+          type: type[color],
+          timer: 2000,
+          placement: {
+              from: from,
+              align: align
+          },
+          template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+            '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+            '<i class="material-icons" data-notify="icon">notifications</i> ' +
+            '<span data-notify="title">{1}</span> ' +
+            '<span data-notify="message">{2}</span>' +
+            '<div class="progress" data-notify="progressbar">' +
+              '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+            '</div>' +
+            '<a href="{3}" target="{4}" data-notify="url"></a>' +
+          '</div>'
+      });
+  }
   }
 
  
