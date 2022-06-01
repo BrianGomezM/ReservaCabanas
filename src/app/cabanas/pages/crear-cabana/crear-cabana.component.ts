949e8767a378e0668c83d3ab7e31d6c9968581de
kquintana <kquintana@actualicese.com>
2022-05-31 22:37:21 -0500

    tabla de imagenes en crear cabaña

--- src/app/cabanas/pages/crear-cabana/crear-cabana.component.ts
+++ src/app/cabanas/pages/crear-cabana/crear-cabana.component.ts
@@ -1,5 +1,6 @@
 import { Component,EventEmitter, OnInit,Output } from '@angular/core';
 import { Cabana } from 'app/cabanas/interfaces/Cabana.interface';
+import { Imagen } from 'app/cabanas/interfaces/imagenes.interface';
 import { CabanasService } from 'app/cabanas/services/cabanas.service';
 import { Router } from '@angular/router';
 
@@ -10,9 +11,14 @@ import { Router } from '@angular/router';
 })
 export class CrearCabanaComponent implements OnInit {
   nombre:string;
-  fileToUpload : File | null = null;
+  fileToUpload : Imagen[]= [];
   imagenes : any[] = [];
   imgURL: any;
+  imagen: Imagen={
+    id_imagen:"",
+    nombre_imagen:"",
+    url_imagen:""
+  }
   cabana:Cabana={
   id_cabana:"",
   nombre_cabana:"",
@@ -54,10 +60,23 @@ export class CrearCabanaComponent implements OnInit {
   }
   cargarImagenes(file:FileList){
     for(let i=0; i<file.length;i++){
-      this.imagenes.push(file.item(i));
-      console.log(file.item(i).name);
+      //reader.onload = ; // Renderizamos la imagen
+      let reader = new FileReader();
+     
+      reader.onloadend = () => {
+        this.imagenes.push(file.item(i));
+        this.imagen={
+          id_imagen:"",
+          nombre_imagen:file.item(i).name,
+          url_imagen:reader.result as string
+        };
+        this.fileToUpload.push(this.imagen);
+      }
+      reader.readAsDataURL(file.item(i));
+      console.log(reader.result);
     }
   }
+  
   subirImagenes(file:FileList){
     let nombre = "prueba2";
     for(let i=0; i<file.length; i++){
@@ -65,7 +84,7 @@ export class CrearCabanaComponent implements OnInit {
       reader.readAsDataURL(file.item(0));
       reader.onloadend = () => {
         this.imagenes.push(reader.result);
-        this.cabanaService.subirImagenes(nombre+"_", reader.result).then(
+        this.cabanaService.subirImagenes(nombre+"_"+Date.now(), reader.result).then(
           urlImagen =>{
             console.log(urlImagen);
           }
