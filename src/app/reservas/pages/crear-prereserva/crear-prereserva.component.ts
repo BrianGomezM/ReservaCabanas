6a4f197a53491280c28649a998c7e0af433f2186
kquintana <cristaldemilcolores@gmail.com>
2022-06-16 01:02:17 -0500

    Crear preservasv1, adelanto crear con algunos datos y se asocia con id cabaña, además lista los ids cabaña

--- src/app/reservas/pages/crear-prereserva/crear-prereserva.component.ts
+++ src/app/reservas/pages/crear-prereserva/crear-prereserva.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CabanasService } from 'app/cabanas/services/cabanas.service';
+import { Reserva } from 'app/reservas/interfaces/reservas.interfaces';
+import { Cabana } from 'app/cabanas/interfaces/Cabana.interface';
+import { ReservasService } from 'app/reservas/services/reservas.service';
 
 @Component({
   selector: 'app-crear-prereserva',
@@ -7,13 +11,49 @@ import { Router } from '@angular/router';
   styleUrls: ['./crear-prereserva.component.css']
 })
 export class CrearPrereservaComponent implements OnInit {
+  reserva:Reserva={
+    id_reserva:"",
+    id_cabana:"",
+    id_cliente:"",
+    valor_reserva:"",
+    descuento:"",
+    idUsuario:"",
+    fecha_inicio:"",
+    fecha_fin:""
+  };
+  cabanas:Cabana[]=[];
 
-  constructor(public router:Router) { }
+  constructor(public router:Router, private cabanaService: CabanasService, private reservasService:ReservasService) { }
 
   ngOnInit(): void {
-    
+    this.cabanaService.getCabanas().subscribe(
+      (cabanas) =>{
+        this.cabanas = cabanas;
+        
+      }
+    );
   }
   redirect(){
     this.router.navigate(["reservas"]);
+  }
+
+  crearpreReserva(){
+    this.reservasService.crearpreReserva(this.reserva).subscribe(
+      resp=>{
+        this.reserva = {
+          id_reserva:"",
+          id_cabana:"",
+          id_cliente:"",
+          valor_reserva:"",
+          descuento:"",
+          idUsuario:"",
+          fecha_inicio:"",
+          fecha_fin:""
+        };
+        this.redirect();
+        console.log(resp.id_reserva);
+        console.log(resp.descuento);
+      }
+    );
   }
 }
--- src/app/reservas/pages/listado-reservas/listado-reservas.component.ts
+++ src/app/reservas/pages/listado-reservas/listado-reservas.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { weekNumber } from 'weeknumber';
-import * as moment from 'moment';
 
 @Component({
   selector: 'app-listado-reservas',
